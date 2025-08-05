b16e0ebcc431f936c7446874271e86ccd226c090
lam.dodang10 <lam.dodang10@mobifone.vn>
2025-08-05 14:56:26 +0700

    cập nhập bảng ngành học

--- app/Providers/BootstrapServiceProvider.php
+++ app/Providers/BootstrapServiceProvider.php
@@ -22,6 +22,8 @@ class BootstrapServiceProvider extends ServiceProvider
         $this->app->bind(\App\Services\Web\Menu\MenuServiceInterface::class, \App\Services\Web\Menu\MenuService::class);
         $this->app->bind(\App\Services\Web\Post\PostServiceInterface::class, \App\Services\Web\Post\PostService::class);
         $this->app->bind(\App\Services\Web\Album\AlbumServiceInterface::class, \App\Services\Web\Album\AlbumService::class);
+     
+        $this->app->bind(\App\Services\Web\Major\MajorServiceInterface::class, \App\Services\Web\Major\MajorService::class);
         //:end-bindings:
     }
 }
